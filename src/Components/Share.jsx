8768cce417a9c4ce063e6e86f653449e7d7d5d8e
omaran bazna <omran4d@gmail.com>
2023-03-31 01:27:40 -0400

    remove the console.log from the code

--- src/Components/Share.jsx
+++ src/Components/Share.jsx
@@ -17,9 +17,9 @@ function Share(){
         try {
           var successful = document.execCommand('copy');
           var msg = successful ? 'successful' : 'unsuccessful';
-          console.log('Fallback: Copying text command was ' + msg);
+        
         } catch (err) {
-          console.error('Fallback: Oops, unable to copy', err);
+         
         }
       
         document.body.removeChild(textArea);
@@ -30,9 +30,9 @@ function Share(){
           return;
         }
         navigator.clipboard.writeText(text).then(function() {
-          console.log('Async: Copying to clipboard was successful!');
+       
         }, function(err) {
-          console.error('Async: Could not copy text: ', err);
+        
         });
       }
 
