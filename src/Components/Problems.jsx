4455d87c8cf8cd626c0fb5faf4c9b85beca1aea7
omaran bazna <omran4d@gmail.com>
2023-03-31 01:47:59 -0400

    add styling to the next and previous buttons

--- src/Components/Problems.jsx
+++ src/Components/Problems.jsx
@@ -25,12 +25,12 @@ function Problems(){
             
                 <Container style={{width:"87%"}} className="mx-auto m-4" fluid> 
                     <Row>
-                        <Col sm={6} md={4}>
+                        <Col sm={5} md={3} className="m-1">
                             {index>0 &&
         <Button style={{width:"100%"}} className="ml-auto" onClick={previousPage}>Previous</Button>}</Col>
-        <Col sm={0} md={4}>
+        <Col sm={2} md={6}  className="m-1">
         </Col>
-        <Col sm={6} md={4}>
+        <Col sm={5} md={3}  className="m-1">
             {index<problemsArr.length-1 &&
          <Button style={{width:"100%"}} className="mr-auto" onClick={nextPage}>Next</Button>
             }
