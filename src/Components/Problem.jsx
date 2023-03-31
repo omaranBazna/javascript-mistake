9c42667a29d4c53205f0620fc7e73edfa9e2df5b
omaran bazna <omran4d@gmail.com>
2023-03-30 21:50:24 -0400

    add code editor

--- src/Components/Problem.jsx
+++ src/Components/Problem.jsx
@@ -1,8 +1,20 @@
 
-
+import Editor from "@monaco-editor/react";
 function Problem({item}){
 
-    return(<h1>{item}</h1>)
+    return(<div><h1>{item}</h1>
+    <div style={{width:"80%",margin:"auto"}}>
+    <Editor
+     style={{border:"4px solid grey"}}
+     height="200px"
+     width="100%"
+     defaultLanguage="javascript"
+     defaultValue={`console.log("helloword")`}
+     language={"javascript"}
+    value={`console.log("hello word")`}
+    theme="vs-dark"
+   /></div>
+    </div>)
 }
 
 export default Problem
